792823bb9f0b9f6d83db3a74824abc10458696db
kenya6565 <ken.australia0605@gmail.com>
2023-03-10 23:46:18 +0900

    create LocalStateA.tsx

---
+++ components/LocalStateA.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ChangeEvent, FormEvent, useState, VFC } from 'react'
+import { todoVar } from '../cache'
+import { useReactiveVar } from '@apollo/client'
+import Link from 'next/link'
+
+const LocalStateA = () => {
+  return <div></div>
+}
+
+export default LocalStateA
