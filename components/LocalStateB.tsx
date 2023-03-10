9a3966a0d116e3544b18ab6a53406f446e57ddb8
kenya6565 <ken.australia0605@gmail.com>
2023-03-11 01:56:24 +0900

    fix LocalStateB.tsx

--- components/LocalStateB.tsx
+++ components/LocalStateB.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { todoVar } from '../cache'
+import { useReactiveVar } from '@apollo/client'
+import Link from 'next/link'
 
 const LocalStateB = () => {
-  return <div></div>
+  const todos = useReactiveVar(todoVar)
+  return (
+    <>
+      {todos?.map((task, index) => {
+        ;<p className="mb-3" key={index}>
+          {task.title}
+        </p>
+      })}
+      <Link href="/local-state-a">
+        <a>Back</a>
+      </Link>
+    </>
+  )
 }
 
 export default LocalStateB
