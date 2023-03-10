9f098a5ae8ef232fa81d3c75ee803a71590bf0cf
kenya6565 <ken.australia0605@gmail.com>
2023-03-11 01:55:31 +0900

    create local-state-b.tsx

---
+++ pages/local-state-b.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LocalStateB from '../components/LocalStateA'
+import { Layout } from '../components/Layout'
+
+const LocalStatePageB = () => {
+  return (
+    <Layout title="Local State B">
+      <LocalStateB />
+    </Layout>
+  )
+}
+
+export default LocalStatePageB
