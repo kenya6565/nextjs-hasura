ec20e952abd3d7a4375ede6b352d2cbe0ded2cb7
kenya6565 <ken.australia0605@gmail.com>
2023-03-13 23:39:23 +0900

    make it available to use apolloClient in every file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { initializeApollo } from '../lib/apolloClient'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const client = initializeApollo()
+  return (
+    // make it available to use apolloClient in every file
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
