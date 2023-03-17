a73be5f47667ffda40a47fdc60dda5d7f37c06b7
kenya6565 <ken.australia0605@gmail.com>
2023-03-18 00:42:49 +0900

    add explanation of fetchPolicy

--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -6,12 +6,21 @@ import { useQuery } from '@apollo/client'
 
 const FetchMain = () => {
   const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
-    // store latest data as cache by accessing graphql, every time executing useQuery
-    fetchPolicy: 'network-only',
-    // fetchPolicy: 'cache-and-network',
-    //fetchPolicy: 'cache-first',
+    // store latest data as cache by accessing graphql(server side), every time executing useQuery
+    // effective if you often change data in serverside or you want latest data real time
+    // fetchPolicy: 'network-only',
+
+    // similar to net-only
+    // the same that store latest data as cache by accessing graphql(server side), every time executing useQuery
+    // but, it displays the existing data first and then displays updated data when getting prepared
+    fetchPolicy: 'cache-and-network',
 
     // default when none is set
+    // always read it when there is data you got as cache
+    // you should avoid it if you often change data in server side
+    // fetchPolicy: 'cache-first',
+
+    // it doesn't create any cache
     //fetchPolicy: 'no-cache',
   })
   if (error)
