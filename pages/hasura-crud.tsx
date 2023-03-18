3059c64db969eb800ca6f66351209845cfe5ef35
kenya6565 <ken.australia0605@gmail.com>
2023-03-18 18:56:08 +0900

    add  useMutation, create, update, delete user

--- pages/hasura-crud.tsx
+++ pages/hasura-crud.tsx
@@ -18,4 +18,58 @@ import {
 } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
 
-const HasuraCRUD = () => {}
+const HasuraCRUD = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
+    fetchPolicy: 'cache-and-network',
+  })
+
+  // get update_users_bu_pk as return value to execute update user
+  // it is function
+  const [update_users_by_pk] = useMutation<UpdateUserMutation>(UPDATE_USER)
+
+  // create and delete are needed to update cache by yourself
+  const [insert_users_one] = useMutation<CreateUserMutation>(CREATE_USER, {
+    // process to deal with cache
+    // insert_users_one is created user now
+    update(cache, { data: { insert_users_one } }) {
+      // get created user's id by cache.identify
+      const cacheId = cache.identify(insert_users_one)
+      // update field by cache.modify
+      cache.modify({
+        // select which fields you want to update
+        fields: {
+          // get current data of user by existingUsers as array
+          users(existingUsers, { toReference }) {
+            // refer to data of created user by toReference(cacheId)
+            // unfold current data of user by spread syntax and add created user at the top
+            return [toReference(cacheId), ...existingUsers]
+          },
+        },
+      })
+    },
+  })
+
+  const [delete_users_by_pk] = useMutation<DeleteUserMutation>(DELETE_USER, {
+    // delete_users_by_pk is deleted user now
+    update(cache, { data: { delete_users_by_pk } }) {
+      cache.modify({
+        // delete target user from existingUsers by filter
+        fields: {
+          // readField allows you to read any fields you select(id in this case)
+          users(existingUsers, { readField }) {
+            return existingUsers.filter(
+              // leave user who does not match user's id which got deleted now
+              (user) => delete_users_by_pk.id !== readField('id', user)
+            )
+          },
+        },
+      })
+    },
+  })
+
+  return (
+    <Layout title="Hasura CRUD">
+      <p className="mb-3 font-bold">Hasura CRUD</p>
+    </Layout>
+  )
+}
