af9f109f87a14eeef83a211f90b0ff30d2845bfa
kenya6565 <ken.australia0605@gmail.com>
2023-03-19 21:38:02 +0900

    bring necessary elements as Props UserItem.tsx

--- components/UserItem.tsx
+++ components/UserItem.tsx
@@ -1,4 +1,28 @@
 import { memo, Dispatch, SetStateAction } from 'react'
 import { Users, DeleteUserMutationFn } from '../types/generated/graphql'
 
-export const UserItem = (Props) => {}
+interface Props {
+  user: {
+    __typename?: 'users'
+    // select type id, name, created_at by PICK
+  } & Pick<Users, 'id' | 'name' | 'created_at'>
+  delete_users_by_pk: DeleteUserMutationFn
+  setEditedUser: Dispatch<
+    SetStateAction<{
+      id: string
+      name: string
+    }>
+  >
+}
+export default function UserItem({
+  user,
+  delete_users_by_pk,
+  setEditedUser,
+}: Props) {
+  return (
+    <div className="my-1">
+      <p className="mr-2">{user.name}</p>
+      <p className="mr-2">{user.created_at}</p>
+    </div>
+  )
+}
