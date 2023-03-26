c1b7f38c18fc3432b34fac48b59f7ab45cacfd22
kenya6565 <ken.australia0605@gmail.com>
2023-03-26 23:25:32 +0900

    create useCreateForm.ts

---
+++ hooks/useCreateForm.ts
@@ -0,0 +1,6 @@
+import { useState, useCallback, ChangeEvent, FormEvent } from 'react'
+import { useMutation } from '@apollo/client'
+import { CREATE_USER } from '../queries/queries'
+import { CreateUserMutation } from '../types/generated/graphql'
+
+export const useCreateForm = () => {}
