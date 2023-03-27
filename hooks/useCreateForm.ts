5988bf5b56cccc9e40cf789da46940a1b98dc4ad
kenya6565 <ken.australia0605@gmail.com>
2023-03-28 01:57:26 +0900

    add insert_users_one

--- hooks/useCreateForm.ts
+++ hooks/useCreateForm.ts
@@ -6,4 +6,25 @@ import { CreateUserMutation } from '../types/generated/graphql'
 export const useCreateForm = () => {
   const [text, setText] = useState('')
   const [username, setUsername] = useState('')
+  // create and delete are needed to update cache by yourself
+  const [insert_users_one] = useMutation<CreateUserMutation>(CREATE_USER, {
+    // process to deal with cache
+    // insert_users_one is created user now
+    update(cache, { data: { insert_users_one } }) {
+      // get created user's id by cache.identify
+      const cacheId = cache.identify(insert_users_one)
+      // update field by cache.modify
+      cache.modify({
+        // select which fields you want to update
+        fields: {
+          // get current data of user by existingUsers as array
+          users(existingUsers, { toReference }) {
+            // refer to data of created user by toReference(cacheId)
+            // unfold current data of user by spread syntax and add created user at the top
+            return [toReference(cacheId), ...existingUsers]
+          },
+        },
+      })
+    },
+  })
 }
