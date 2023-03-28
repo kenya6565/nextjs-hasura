17034caf0edf05d51c776f8ef8bc32b657aec8f3
kenya6565 <ken.australia0605@gmail.com>
2023-03-29 02:00:52 +0900

    add button

--- components/Child.tsx
+++ components/Child.tsx
@@ -5,7 +5,18 @@ interface Props {
 }
 
 const Child = ({ printMsg }) => {
-  return <div>Child</div>
+  return (
+    <>
+      {/* {console.log('Child rendered')} */}
+      <p>Child Component</p>
+      <button
+        className="my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+        onClick={printMsg}
+      >
+        click
+      </button>
+    </>
+  )
 }
 
 export default Child
