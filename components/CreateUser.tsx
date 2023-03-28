2a54d07d2df2165c51d05953f8d983fb62583264
kenya6565 <ken.australia0605@gmail.com>
2023-03-29 01:53:47 +0900

    add input tag

--- components/CreateUser.tsx
+++ components/CreateUser.tsx
@@ -11,6 +11,22 @@ const CreateUser = () => {
     text,
     handleTextChange,
   } = useCreateForm()
+
+  return (
+    <>
+      {console.log('CreateUser rendered')}
+      <p className="mb-3 font-bold">Custom Hook + useCallback + memo</p>
+      <div className="mb-3 flex flex-col justify-center items-center">
+        <label>Text</label>
+        <input
+          className="px-3 py-2 border border-gray-300"
+          type="text"
+          value={text}
+          onChange={handleTextChange}
+        />
+      </div>
+    </>
+  )
 }
 
 export default CreateUser
